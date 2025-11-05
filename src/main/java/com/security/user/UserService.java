5c6fe7ff740b48806a3732f189ef8c920bbc47b7
cuong <loutrinh2312000@gmail.com>
2025-11-05 10:55:02 +0700

    update

---
+++ src/main/java/com/security/user/UserService.java
@@ -0,0 +1,16 @@
+package com.security.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepo userRepo;
+
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+}
